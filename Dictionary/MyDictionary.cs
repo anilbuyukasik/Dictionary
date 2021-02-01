062f95741678e4324c8c510dd5bc0145a6255853
Anıl Büyükaşık <buyukasik.anil@gmail.com>
2021-02-01 21:31:39 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<XKey, XVal>
+    {
+        XKey[] keys;
+        XVal[] vals;
+        string result;
+
+        public MyDictionary()
+        {
+            keys = new XKey[0];
+            vals = new XVal[0];
+        }
+        public void Add(XKey key, XVal val)
+        {
+            XKey[] tempArrayKey = keys;
+            keys = new XKey[keys.Length + 1];
+
+            XVal[] tempArrayVal = vals;
+            vals = new XVal[vals.Length + 1];
+
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                keys[i] = tempArrayKey[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            for (int i = 0; i < tempArrayVal.Length; i++)
+            {
+                vals[i] = tempArrayVal[i];
+            }
+            vals[vals.Length - 1] = val;
+
+        }
+        public int Length
+        {
+            get { return keys.Length; }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> aylar = new MyDictionary<int, string>();
+            aylar.Add(1, "Ocak");
+            aylar.Add(2, "Şubat");
+            aylar.Add(3, "Mart");
+
+            Console.WriteLine(aylar.Length);
+        }
+    }
+}
